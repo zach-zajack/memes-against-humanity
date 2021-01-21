942074e8e680fc88ef3cc48ffe79b43edd042681
Zach Zajack <zachzajack@gmail.com>
2021-01-21 14:25:21 -0500

    :sparkles: Basic logic for creating players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,30 @@
 class PlayersController < ApplicationController
   def new
+    @player = Player.new
+  end
+
+  def create
+    @game ||= Game.new
+    @player = Player.new(player_params.merge(game: @game))
+
+    respond_to do |format|
+      if @player.save
+        session[:player_id] = @player.id
+        @game.save
+        format.html { redirect_to @game }
+        format.json { render :show, status: :created, location: @game }
+      else
+        format.html { render :new }
+        format.json do
+          render json: @player.errors, status: :unprocessable_entity
+        end
+      end
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name)
   end
 end
