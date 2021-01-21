ff240863ec77081391eb6895ae8e77a42898a3bd
Zach Zajack <zachzajack@gmail.com>
2021-01-21 14:24:21 -0500

    :construction: Update routes for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :players, only: [:new, :create]
+  resources :games, param: :join_code, except: [:new, :index, :edit]
+
+  root to: "players#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
