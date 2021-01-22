a093a6ab9f3ab65ea288771474e21ef81feb777d
Zach Zajack <zachzajack@gmail.com>
2021-01-22 16:54:47 -0500

    :fire: Remove variable format responses

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,21 +6,12 @@ class PlayersController < ApplicationController
   def create
     @game ||= Game.new
     @player = Player.new(player_params.merge(game: @game))
-
-    respond_to do |format|
-      if @player.save
-        cookies.signed[:player_id] = @player.id
-        @game.save
-        format.html { redirect_to game_path(@game.join_code) }
-        format.json do
-          render :show, status: :created, location: game_path(@game.join_code)
-        end
-      else
-        format.html { render :new }
-        format.json do
-          render json: @player.errors, status: :unprocessable_entity
-        end
-      end
+    if @player.save
+      cookies.signed[:player_id] = @player.id
+      @game.save
+      redirect_to game_path(@game.join_code)
+    else
+      render :new
     end
   end
 
