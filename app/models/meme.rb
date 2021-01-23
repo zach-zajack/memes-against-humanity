c32aac12f37d0ea9486b809a9f78e0ef0f3e6791
Zach Zajack <zachzajack@gmail.com>
2021-01-23 18:48:11 -0500

    :construction: Meme logic

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -1,4 +1,17 @@
 class Meme < ApplicationRecord
-  belongs_to :player
   belongs_to :round
+
+  validates_presence_of :source1_id
+
+  def author
+    self.source1_id.player
+  end
+
+  def sources
+    [
+      Source.find_by(id: self.source1_id),
+      Source.find_by(id: self.source2_id),
+      Source.find_by(id: self.source3_id)
+    ].compact
+  end
 end
