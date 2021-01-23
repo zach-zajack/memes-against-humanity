b620f6849802d0f9fa1bd400085f2c3fee0f51b0
Zach Zajack <zachzajack@gmail.com>
2021-01-23 15:17:47 -0500

    :sparkles: Basic game UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_player
+
+  def current_player
+    @current_player ||= Player.find_by id: cookies.signed[:player_id]
+  end
+  
   def record_not_found
     raise ActiveRecord::RecordNotFound
   end
