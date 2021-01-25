378a2e5278b028514d001488456b562c835abd6f
Zach Zajack <zachzajack@gmail.com>
2021-01-25 01:51:24 -0500

    :construction: Prevent creating memes with too few or too many sources

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -4,7 +4,7 @@ class Meme < ApplicationRecord
   delegate :game,     to: :round
   delegate :template, to: :round
 
-  validates :source1_id, presence: true
+  validate :source_count_matches_template
 
   after_create_commit :broadcast_meme
 
@@ -16,9 +16,21 @@ class Meme < ApplicationRecord
     ].compact
   end
 
+  def source_ids
+    [self.source1_id, self.source2_id, self.source3_id].compact
+  end
+
   private
 
   def broadcast_meme
     BroadcastGameJob.perform_later(game, ["memes", "scoreboard"])
+  end
+
+  def source_count_matches_template
+    if source_ids.count < template.slots
+      errors.add(:base, "not enough sources")
+    elsif source_ids.count > template.slots
+      errors.add(:base, "too many sources")
+    end
   end
 end
