55d73d7042649d8cea00a1513c17784d3d07a1de
Zach Zajack <zachzajack@gmail.com>
2021-01-30 12:15:33 -0500

    :recycle: Move logic to games helper

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,5 +3,20 @@ module GamesHelper
     return "czar"    if player.czar?
     return "playing" if player.playing?
     return "winner"  if player.winner?
+    return ""
+  end
+
+  def hand_classes(player)
+    return "noclick czar-hand" if player.czar?
+    return "noclick"      if player.ready?
+    return ""
+  end
+
+  def display_meme?(player)
+    player.ready? || player.czar?
+  end
+
+  def display_template?(player)
+    !display_meme?(player)
   end
 end
