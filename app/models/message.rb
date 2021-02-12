802040073ab4b4dad6d12788f844d290604107c1
Zach Zajack <zachzajack@gmail.com>
2021-02-12 01:06:46 -0500

    :lock: Ensure messages aren't being sent too quickly

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,17 @@ class Message < ApplicationRecord
   after_create_commit { BroadcastGameJob.perform_later(player.game, :message) }
 
   validates_length_of :content, minimum: 1, maximum: 200
+  validate :spam
 
   def author
     player.name
+  end
+
+  private
+
+  def spam
+    last_msg = player.messages.last
+    return if last_msg.nil? || Time.now - last_msg.created_at > 0.5.seconds
+    errors.add(:base, "you are sending messages too quickly")
   end
 end
