fc8806ed3bcff6e2ba699a360c4cb36bac95faf6
zatchiian <zachzajack@gmail.com>
2021-08-08 17:40:46 -0400

    :lipstick: Begin mobile UI

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -13,6 +13,10 @@ function initGame() {
   scrollMessages();
 }
 
+function isMobile() {
+  return window.innerHeight > window.innerWidth;
+}
+
 function bindImageFunction(el, fun) {
   el.off();
   el.on("load", fun).each(function() {
@@ -39,30 +43,73 @@ function scrollMessages() {
 }
 
 function resizeGame() {
-  // Subtract 10px for padding
-  var scaleHand = (window.innerWidth - 10) / $(".hand-resize").width();
-  $(".hand-resize").css({transform: "scale("+scaleHand+")"});
+  var handWidth = $(".hand-resize").width() + $(".hand-resize2").width();
+  var handScale = (window.innerWidth - 10) / handWidth;
 
-  $(".bottom").height($(".hand-resize").height() * scaleHand - 10);
-
+  // TODO: really not a fan of this hacky DOM-manipulation garbage,
+  //       but I can't think of a better solution right now
+  if (handScale < 1) {
+    if($(".hand-resize2").length == 0) {
+      $(".hand-resize").after("<div class='hand-resize2'></div>");
+    }
+    
+    var handWidthTotal = 0;
+    $(".hand-resize").children().each(function() {
+      if(handWidthTotal * 2.5 > handWidth) {
+        $(".hand-resize2").append($(this));
+      } else {
+        handWidthTotal += $(this).width();
+      }
+      $(".hand-resize").css({ transform: "translateY(-82px)" });
+    });
+    handScale = (window.innerWidth - 10) / Math.max(handWidthTotal, handWidth - handWidthTotal);
+    handScale = Math.max(handScale, 1);
+    $(".bottom").height($(".hand-resize").height() * handScale * 2 - 10);
+    $(".hand-resize").css({ transform: "scale(" + handScale + ") translateY(-82px)" });
+    $(".hand-resize2").css({ transform: "translateY(-82px) scale(" + handScale + ")" });
+  } else {
+    if($(".hand-resize2").length == 1) {
+      $(".hand-resize2").children().each(function (i) { $(".hand-resize").append($(this)); });
+      $(".hand-resize2").remove();
+    }
+    $(".bottom").height($(".hand-resize").height() * handScale - 10);
+    $(".hand-resize").css({ transform: "scale(" + handScale + ")" });
+  }
+  
   // Subtract 60px for padding (20px) + head (40px)
   var mainHeight = window.innerHeight - $(".bottom").height() - 60;
+
   $(".main").height(mainHeight);
-  $(".sidebar").height(mainHeight);
-  $(".messages").height(mainHeight - $(".scoreboard").outerHeight() - 90);
+  $(".buttons").css({ top: mainHeight });
+
+  if(isMobile()) {
+    $(".main").css({ top: $(".scoreboard").outerHeight() + 40 });
+    $(".sidebar").height($(".scoreboard").outerHeight());
+  } else {
+    $(".main").css({ top: 40 });
+    $(".sidebar").height(mainHeight);
+    $(".messages").height(mainHeight - $(".scoreboard").outerHeight() - 90);
+  }
 
   var heightRatio = $(".main").height() / $("#main").outerHeight();
   var widthRatio = $(".main").width() / $("#main").outerWidth();
   var scale = Math.min(heightRatio, widthRatio);
-  var midpoint = ($(".main").width() - $("#main").outerWidth()*scale) / 2
-  $("#main").css({transform: "scale("+scale+")", left: midpoint});
+  var midpoint = ($(".main").width() - $("#main").outerWidth() * scale) / 2
+  $("#main").css({ transform: "scale(" + scale + ")", left: midpoint });
 }
 
 function selectSource() {
-  var slots = $(".placeholder").length
+  var slots = $(".placeholder").length;
   var source_sel = "[class^='hand-img source']";
   var sources = $(source_sel).length;
   var classname = "source" + sources;
+
+  if(slots == 1 && sources == 1) {
+    $(".source1").removeClass("source1");
+    $(this).addClass("source1");
+    return;
+  }
+
   if($(this).is(source_sel) && $(this).hasClass(classname)) {
     $(this).removeClass(classname);
     sources--;
